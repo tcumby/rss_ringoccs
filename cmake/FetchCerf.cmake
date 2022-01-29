0b61ecb341b1b2eaaab68923d963ba9b7e9895b3
Ty <ty.cumby@gmail.com>
2022-01-29 10:39:00 -0700

    Add missing include for FetchContent

--- cmake/FetchCerf.cmake
+++ cmake/FetchCerf.cmake
@@ -1,6 +1,6 @@
 cmake_minimum_required(VERSION 3.20)
 include_guard()
-
+include(FetchContent)
 FetchContent_Declare(
     cerf_checkout
     GIT_REPOSITORY https://jugit.fz-juelich.de/mlz/libcerf.git
