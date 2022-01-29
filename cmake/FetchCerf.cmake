17da3a4159ba3db78ae6e58aff3b8709eb652ef0
Ty <ty.cumby@gmail.com>
2022-01-28 23:13:43 -0700

    Set up builds for complex_tests and inclusion of libcerf

---
+++ cmake/FetchCerf.cmake
@@ -0,0 +1,10 @@
+cmake_minimum_required(VERSION 3.20)
+include_guard()
+
+FetchContent_Declare(
+    cerf_checkout
+    GIT_REPOSITORY https://jugit.fz-juelich.de/mlz/libcerf.git
+    GIT_TAG 4f2c6384e12d51ea86302bab0c0cb53b2e948c19 # the commit corresponding to v1.17
+)
+
+FetchContent_MakeAvailable(cerf_checkout)
