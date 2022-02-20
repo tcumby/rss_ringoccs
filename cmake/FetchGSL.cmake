8216f122d295e3194f1952b5edd4401064499447
Ty <ty.cumby@gmail.com>
2022-02-20 11:27:53 -0700

    Unify the find_package() usage for GSL

--- cmake/FetchGSL.cmake
+++ cmake/FetchGSL.cmake
@@ -3,16 +3,13 @@ include_guard()
 
 if (APPLE OR UNIX)
     include(FindGSL)
-    find_package(GSL REQUIRED)
-    if (NOT GSL_FOUND)
-        message(FATAL_ERROR "GSL was not found")
-    endif()
 else()
-    find_package(gsl CONFIG REQUIRED)
-    if (NOT gsl_FOUND)
-        message(FATAL_ERROR "GSL was not found")
-    endif()
+    # vcpkg installs GSL for Windows via the vcpkg.json manifest
 endif()
 
+find_package(GSL REQUIRED)
+if (NOT GSL_FOUND)
+    message(FATAL_ERROR "GSL was not found")
+endif()
 
 
