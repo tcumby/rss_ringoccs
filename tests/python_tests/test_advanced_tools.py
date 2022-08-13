127ad9347ad19e0187dd513c4eff3b60b967e510
Ty <ty.cumby@gmail.com>
2022-08-13 11:08:32 -0600

    Reorganizing the test direcctory so the import mechanism imports the installed package, not the local directory.

--- tests/python_tests/test_advanced_tools.py
+++ tests/python_tests/test_advanced_tools.py
@@ -1,4 +1,6 @@
 from pathlib import Path
+from typing import List, Tuple
+
 import pytest  # type: ignore
 import matplotlib.pyplot as plt  # type: ignore
 import numpy
@@ -85,51 +87,51 @@ class TestAdvancedTools:
         )
 
         # List of regions to plot within the C Ring.
-        RegDict = [
-            ["B1 U1", 74665],
-            ["Mimas 4:1", 74900],
-            ["B3, B4, U2, R-b", 74949],
-            ["B5/U3", 76020],
-            ["B6/U7", 76240],
-            ["B9/V1", 76430],
-            ["76,460", 76460],
-            ["B10", 76530],
-            ["B11/U8", 76730],
-            ["Titan -1:0", 77535],
-            ["Titan -1:0 Cont.", 77565],
-            ["C Ring Ripples", 77725],
-            ["B13/R-e", 80980],
-            ["B14", 81020],
-            ["B15/V2", 82000],
-            ["B16/R-f", 82050],
-            ["B17/R-g", 82200],
-            ["B18/R-h", 83625],
-            ["B19/R-i", 84635],
-            ["B20", 84825],
-            ["B21", 84860],
-            ["B22", 85110],
-            ["B23/R-j", 85450],
-            ["B24/U9", 85485],
-            ["B25/U4", 85510],
-            ["B26/U6", 85530],
-            ["B27/R-d", 85680],
-            ["B28a", 86415],
-            ["B28b", 86445],
-            ["B29", 86575],
-            ["B31", 86590],
-            ["B32/V3", 87185],
-            ["Atlas 2:1", 87650],
-            ["B35", 88745],
-            ["Mimas 6:2", 89890],
-            ["Pand 4:2", 89900],
-            ["B40", 90280],
+        reg_dict: List[Tuple[str, float]] = [
+            ("B1 U1", 74665),
+            ("Mimas 4:1", 74900),
+            ("B3, B4, U2, R-b", 74949),
+            ("B5/U3", 76020),
+            ("B6/U7", 76240),
+            ("B9/V1", 76430),
+            ("76,460", 76460),
+            ("B10", 76530),
+            ("B11/U8", 76730),
+            ("Titan -1:0", 77535),
+            ("Titan -1:0 Cont.", 77565),
+            ("C Ring Ripples", 77725),
+            ("B13/R-e", 80980),
+            ("B14", 81020),
+            ("B15/V2", 82000),
+            ("B16/R-f", 82050),
+            ("B17/R-g", 82200),
+            ("B18/R-h", 83625),
+            ("B19/R-i", 84635),
+            ("B20", 84825),
+            ("B21", 84860),
+            ("B22", 85110),
+            ("B23/R-j", 85450),
+            ("B24/U9", 85485),
+            ("B25/U4", 85510),
+            ("B26/U6", 85530),
+            ("B27/R-d", 85680),
+            ("B28a", 86415),
+            ("B28b", 86445),
+            ("B29", 86575),
+            ("B31", 86590),
+            ("B32/V3", 87185),
+            ("Atlas 2:1", 87650),
+            ("B35", 88745),
+            ("Mimas 6:2", 89890),
+            ("Pand 4:2", 89900),
+            ("B40", 90280),
         ]
 
         # Convert to a string for plots.
-        res = "%s km" % str(TauInst.res)
+        res_label = "%s km" % str(TauInst.res)
 
         # Total number of plots, and total number of pages.
-        N_Plots = len(RegDict)
+        N_Plots = len(reg_dict)
         N_Pages = int(N_Plots / 8.0)
 
         with PdfPages(str(outfile)) as pdf:
@@ -138,7 +140,7 @@ class TestAdvancedTools:
                 plt.rc("font", family="serif")
                 plt.rc("font", size=10)
                 plt.figure(figsize=(8.5, 11))
-                plt.suptitle("{} Reconstructions: {} Resolution".format(rev, res), size=14)
+                plt.suptitle(f"{rev} Reconstructions: {res_label} Resolution", size=14)
 
                 # Use gridspec to create 4-by-2 plots.
                 gs = gridspec.GridSpec(4, 2, wspace=0.5, hspace=0.5)
@@ -160,7 +162,7 @@ class TestAdvancedTools:
                     plt.locator_params(axis="x", nbins=4)
 
                     # Set the title, and labels for both axes.
-                    plt.title(RegDict[int(8 * i_page + i_plot)][0])
+                    plt.title(reg_dict[int(8 * i_page + i_plot)][0])
                     plt.ylabel("Normalized Optical Depth")
                     plt.xlabel("Ring Radius (km)")
 
@@ -169,8 +171,8 @@ class TestAdvancedTools:
                     plt.plot(TauInst.tau_rho, TauInst.tau_tau, "r")
 
                     # Compute the range for the x-axis.
-                    rmin = RegDict[int(8 * i_page + i_plot)][1] - 25.0
-                    rmax = RegDict[int(8 * i_page + i_plot)][1] + 25.0
+                    rmin = reg_dict[int(8 * i_page + i_plot)][1] - 25.0
+                    rmax = reg_dict[int(8 * i_page + i_plot)][1] + 25.0
 
                     # Get the indices that are being plotted.
                     nmin = numpy.min((TauInst.rho_km_vals >= rmin).nonzero())
@@ -196,7 +198,7 @@ class TestAdvancedTools:
                 plt.rc("font", size=10)
                 plt.figure(figsize=(8.5, 11))
                 plt.suptitle(
-                    "{} C-Ring Reconstructions: {} Resolution".format(rev, res), size=14
+                    f"{rev} C-Ring Reconstructions: {res_label} Resolution", size=14
                 )
 
                 # Use gridspec to create 4-by-2 plots.
@@ -218,7 +220,7 @@ class TestAdvancedTools:
                     plt.locator_params(axis="x", nbins=4)
 
                     # Title and labels for axes.
-                    plt.title(RegDict[int(8 * (N_Pages - 1) + i_plot)][0])
+                    plt.title(reg_dict[int(8 * (N_Pages - 1) + i_plot)][0])
                     plt.ylabel("Normalized Power")
                     plt.xlabel("Ring Radius (km)")
 
@@ -227,8 +229,8 @@ class TestAdvancedTools:
                     plt.plot(TauInst.tau_rho, TauInst.tau_tau, "r")
 
                     # Compute the range of the x and y axes.
-                    rmin = RegDict[int(8 * (N_Pages - 1) + i_plot)][1] - 15.0
-                    rmax = RegDict[int(8 * (N_Pages - 1) + i_plot)][1] + 15.0
+                    rmin = reg_dict[int(8 * (N_Pages - 1) + i_plot)][1] - 15.0
+                    rmax = reg_dict[int(8 * (N_Pages - 1) + i_plot)][1] + 15.0
 
                     # Get the indices that are being plotted.
                     nmin = numpy.min((TauInst.rho_km_vals >= rmin).nonzero())
